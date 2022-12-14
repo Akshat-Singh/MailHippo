3b2e28e47e82595fbfc85b9a9737c4d185817693
Vibodh Nautiyal <vibodh.nautiyal_ug22@ashoka.edu.in>
2022-12-14 14:17:27 +0530

    Google login functionality

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -5,33 +5,42 @@ import PageFormat from './PageFormat.js'
 
 export default function Login() {
 
-  //   const [user, setUser] = useState({});
+    const [user, setUser] = useState({});
 
-  //   function handleCallbackResponse(response) {
-  //       console.log("Encoded JWT ID token: " + response.credential);
-  //       var userObject = jwt_decode(response.credential);
-  //       console.log(userObject);
-  //       setUser(userObject);
-  //       document.getElementById("signInDiv").hidden = true;
-  //   }
+    function handleCallbackResponse(response) {
+        console.log("Encoded JWT ID token: " + response.credential);
+        var userObject = jwt_decode(response.credential);
+        console.log(userObject);
+        setUser(userObject);
+        document.getElementById("signInDiv").hidden = true;
+    }
 
-  //   useEffect(() => {
-  //       /* global google */
-  //       google.accounts.id.initialize({
-  //       client_id: "16473489388-a8lvnk37eu5o5l16mqd7j21fqf89etct.apps.googleusercontent.com",
-  //       callback: handleCallbackResponse,
-  //       });
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+        client_id: "16473489388-a8lvnk37eu5o5l16mqd7j21fqf89etct.apps.googleusercontent.com",
+        callback: handleCallbackResponse,
+        });
 
-  //   google.accounts.id.renderButton(
-  //     document.getElementById("signInDiv"),
-  //     {theme: "outline", size: "large"}
-  //   );
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    );
 
-  // }, []);
+  }, []);
 
     return (
 
+      <div>
+
+        <div className = "App">
+          <div id = "signInDiv"> </div>
+        </div>
+
         <PageFormat />
+
+
+      </div>
 
     );
 }
